85ade99e47f24b2b6e7277e5499d9caade8bef90
Amit <amit@Amits-MBP.alphionsee.in>
2023-09-07 23:09:15 +0530

    removed unused code

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import Content from "./routes/Home/Content";
 import { Outlet } from "react-router-dom";
 function App() {
   return (
